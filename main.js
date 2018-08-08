92d1cff0e2270a439b0eba8aca3f95ed77e0ba94
Valentin Steblyk <steblykv@gmail.com>
2018-08-08 16:57:40 +0300

    Second solution

--- main.js
+++ main.js
@@ -1,70 +1,51 @@
-let institution = (function(){
-    let students = [{ 
-        name: "Valentin",
-        mark: 86
-    },
-    { 
-        name: "Paul",
-        mark: 94
-    },
-    { 
-        name: "Olia",
-        mark: 80
-    },
-    { 
-        name: "Tania",
-        mark: 71
-    },
-    { 
-        name: "Natalia",
-        mark: 83
-    },
-    { 
-        name: "Petro",
-        mark: 90
-    },
-    { 
-        name: "Dima",
-        mark: 65
-    },
-    { 
-        name: "Ira",
-        mark: 78
-    },
-    ];
-
-    let intership = [];
-    let sum = 0;
-    let avgMark = 0;
-    let mrk;
-
-    for (i=0; i<students.length; i++) {
-        mrk = students[i].mark;
-        sum += mrk;       
-    };
-    avgMark = sum/students.length;
-
-    let filterStudents = students.filter(studentMark => {
-        return studentMark.mark > mrk;
-    });
-
-    return {     
-        addStudents: function() {
-                intership.push(filterStudents);
-
-        },
-            
-        display: function(){
-            console.log("List of all students: ", ...students);
-            
-            for (i=0; i<students.length; i++) {
-                if (intership[0][i] !== undefined) {
-                    console.log("Student with a higher mark than average: ", intership[0][i]);
-                }
-            }  
-        }
+class Student{
+    constructor(name, mark){
+        this.name = name;
+        this.mark = mark;
     }
-})();
+    display(){
+        console.log(this.name, this.mark);
+    }
+}
 
-institution.addStudents();
-institution.display();
+let intership = [];
+let allStudents = [];
+
+let tom = new Student("Tom", 78);
+let tim = new Student("Tim", 67);
+let adam = new Student("Adam", 85);
+let alice = new Student("Alice", 94);
+let kim = new Student("Kim", 81);
+
+allStudents.push(tom, tim, adam, alice, kim);
+
+let sum = 0;
+let avgMark = 0;
+let mrk;
+
+for (i=0; i<allStudents.length; i++) {
+   mrk = allStudents[i].mark;
+   sum += mrk;       
+};
+avgMark = sum/allStudents.length;
+
+let filterStudents = allStudents.filter(studentMark => {
+    return studentMark.mark > mrk;
+});
+
+function addStudents(){
+    intership.push(filterStudents);
+}
+
+function display(){
+    console.log("List of all students: ", ...allStudents);
+    
+    for (i=0; i<allStudents.length; i++) {
+        if (intership[0][i] !== undefined) {
+            console.log("Student with a higher mark than average: ", intership[0][i]);
+        }
+    }  
+}
+
+addStudents();
+display();
